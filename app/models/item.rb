2612162d94078379e47e9bab63671c6d39dedbfb
FootLovePeace <soccer.26.promise@gmail.com>
2020-11-13 18:01:06 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,17 @@
 class Item < ApplicationRecord
   beloｎgs_to :user
   has_one_attached :image
-end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :ship_date
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+  end
+end 
